d479e93b5f67b0df44b8b9eec7187ad0da2d9a72
neelk <neelesh.karthi@gmail.com>
2024-06-22 19:42:42 -0700

    integrated kafka

--- script.sh
+++ script.sh
@@ -1,14 +1,12 @@
 #!/bin/zsh
 
 # Remove existing Docker containers and images if they exist
+docker rm -f read_volume || true
 docker rm -f camera_traps || true
 docker rm -f capture_daemon || true
+docker rmi -f read_volume || true
 docker rmi -f camera_traps || true
 docker rmi -f capture_daemon || true
-
-# Remove Docker network and volume if they exist
-docker network rm cpu_network || true
-docker volume rm icicle || true
 
 # Clear all Docker cache and prune all unused Docker objects
 docker system prune -af --volumes
@@ -28,23 +26,24 @@ docker build -t camera_traps .
 cd ../capture_daemon
 docker build -t capture_daemon .
 
+# Navigate to read_volume directory and build the image
+cd ../read_volume
+docker build -t read_volume .
+
 # Create Docker network
-docker network create cpu_network || true
+docker network create ckn || true
 
-# Run new containers
-docker run -d --name camera_traps --network cpu_network -v icicle:/data camera_traps
-docker run -d --name capture_daemon --network cpu_network -v icicle:/data capture_daemon
+# Start Kafka and Zookeeper using docker-compose
+docker-compose up -d
 
-# Follow the logs of the camera_traps and capture_daemon containers
-docker logs -f camera_traps &
-docker logs -f capture_daemon &
+# Run the containers with the volume mounted
+docker run -d --name camera_traps --network ckn -v icicle:/data camera_traps
+docker run -d --name capture_daemon --network ckn -v icicle:/data capture_daemon
+docker run -d --name read_volume --network ckn -v icicle:/data read_volume
+
+# Follow the logs of the read_volume container
+docker logs -f read_volume
 
 # Optionally delete the Docker network and volume if they exist after stopping the containers
-# docker network rm cpu_network || true
+# docker network rm ckn || true
 # docker volume rm icicle || true
-
-# Wait for 60 seconds
-sleep 20
-
-# Terminate the capture_daemon container
-docker stop capture_daemon
