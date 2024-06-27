a5ed61ebce65827e727cb56737a5c0dd4e23e53b
Sachith Withana <swsachith@gmail.com>
2024-06-27 11:45:53 -0400

    adding oracle generator and ckn daemon using kafka for publishing

---
+++ oracle_ckn_daemon/oracle_daemon.py
@@ -0,0 +1,82 @@
+import csv
+import logging
+import time
+import json
+import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+from confluent_kafka import Producer, KafkaError
+
+ORACLE_EVENTS_FILE = os.getenv('ORACLE_CSV_PATH', '/Users/swithana/git/d2i/icicle-ckn/oracle_ckn_daemon/output.csv')
+CKN_LOG_FILE = os.getenv('CKN_LOG_FILE', './ckn_daemon.log')
+KAFKA_BROKER = os.getenv('CKN_KAFKA_BROKER', 'localhost:9092')
+KAFKA_TOPIC = os.getenv('CKN_KAFKA_TOPIC', 'oracle_events')
+DEVICE_ID = os.getenv('CAMERA_TRAPS_DEVICE_ID', 'device_1')
+
+
+header = ['image_count', 'UUID', 'image_name', 'image_receiving_timestamp', 'image_scoring_timestamp', 'score_label', 'score_probability', 'image_store_delete_time', 'image_decision']
+
+
+class OracleEventHandler(FileSystemEventHandler):
+    def __init__(self, file_path, producer, topic, device_id):
+        self.file_path = file_path
+        self.producer = producer
+        self.topic = topic
+        self.device_id = device_id
+        self.processed_lines = set()
+
+    def on_deleted(self, event):
+        pass
+
+    def on_modified(self, event):
+        if event.src_path == self.file_path:
+            print(f"File {self.file_path} modified.")
+            self.read_new_lines()
+
+    def read_new_lines(self):
+        print(f"Reading new lines from {self.file_path}")
+        with open(self.file_path, mode='r') as file:
+            reader = csv.DictReader(file, fieldnames=header)
+            for line_number, row in enumerate(reader, start=1):
+                if line_number not in self.processed_lines:
+                    self.processed_lines.add(line_number)
+                    print(f"New row: {row}")  # or process the row as needed
+                    self.produce_event(row)
+
+    def produce_event(self, event):
+        try:
+            event['device_id'] = self.device_id
+            row_json = json.dumps(event)
+            self.producer.produce(self.topic, key=DEVICE_ID, value=row_json)
+            self.producer.flush()
+        except BufferError as e:
+            logging.error(f"Buffer error: {e}")
+        except KafkaError as e:
+            logging.error(f"Kafka error: {e}")
+
+
+if __name__ == "__main__":
+
+    if not os.path.exists(ORACLE_EVENTS_FILE):
+        print(f"File {ORACLE_EVENTS_FILE} does not exist. Creating it.")
+        open(ORACLE_EVENTS_FILE, 'w').close()
+
+    logging.basicConfig(filename=CKN_LOG_FILE, level=logging.INFO, format='%(asctime)s - %(message)s')
+
+    conf = {'bootstrap.servers': KAFKA_BROKER}
+    producer = Producer(**conf)
+
+    event_handler = OracleEventHandler(file_path=ORACLE_EVENTS_FILE, producer=producer, topic=KAFKA_TOPIC, device_id=DEVICE_ID)
+    observer = Observer()
+    observer.schedule(event_handler, path=os.path.dirname(ORACLE_EVENTS_FILE), recursive=False)
+
+    print(f"Watching file: {ORACLE_EVENTS_FILE}")
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
---
+++ oracle_ckn_daemon/oracle_generator.py
@@ -0,0 +1,68 @@
+import csv
+import time
+from datetime import datetime
+import uuid
+import random
+
+#
+#This class is used as a stub to generate oracle data for CKN testing.
+#
+
+header = ['image_count', 'UUID', 'image_name', 'image_receiving_timestamp', 'image_scoring_timestamp', 'score_label',
+          'score_probability', 'image_store_delete_time', 'image_decision']
+data_template = {
+    "image_count": 1,
+    "UUID": "",
+    "image_name": "/example_images/labrador-pup.jpg",
+    "image_receiving_timestamp": "",
+    "image_scoring_timestamp": "",
+    "score_label": "animal",
+    "score_probability": 0.8429999947547913,
+    "image_store_delete_time": "",
+    "image_decision": "Save"
+}
+
+# Open the CSV file for writing
+with open('./output.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    # writer.writerow(header)  # Write the header
+
+    i = 1
+    while True:
+        # Update the dynamic parts of the data
+        current_time = datetime.utcnow().isoformat() + "Z"
+        unique_id = str(uuid.uuid4())
+        score_probability = round(random.uniform(0.5, 1.0), 6)
+
+        data_template["UUID"] = unique_id
+        data_template["image_count"] = i
+        data_template["image_receiving_timestamp"] = current_time
+        data_template["image_scoring_timestamp"] = current_time
+        data_template["score_probability"] = score_probability
+        data_template["image_store_delete_time"] = current_time
+
+        # increasing the image count by 1
+        i += 1
+
+        # Prepare the row data
+        row = [
+            data_template["image_count"],
+            data_template["UUID"],
+            data_template["image_name"],
+            data_template["image_receiving_timestamp"],
+            data_template["image_scoring_timestamp"],
+            data_template["score_label"],
+            data_template["score_probability"],
+            data_template["image_store_delete_time"],
+            data_template["image_decision"]
+        ]
+
+        # Write the row to the CSV file
+        writer.writerow(row)
+        print(f'Written row to CSV: {row}')
+
+        # Flush the file to ensure data is written
+        file.flush()
+
+        # Wait for one second
+        time.sleep(1)
