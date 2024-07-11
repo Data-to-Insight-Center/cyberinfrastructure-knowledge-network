500a729db8eef7826103074a8b6397ff01245129
Sachith Withana <swsachith@gmail.com>
2024-07-11 14:21:33 -0400

    changed logging and added comments

--- oracle_ckn_daemon/oracle_daemon.py
+++ oracle_ckn_daemon/oracle_daemon.py
@@ -9,7 +9,7 @@ from confluent_kafka import Producer, KafkaError
 
 ORACLE_EVENTS_FILE = os.getenv('ORACLE_CSV_PATH', '/Users/swithana/git/icicle/camera-traps/releases/0.3.3/oracle_plugin_dir/image_mapping_final.json')
 CKN_LOG_FILE = os.getenv('CKN_LOG_FILE', './ckn_daemon.log')
-KAFKA_BROKER = os.getenv('CKN_KAFKA_BROKER', '<IP>:9092')
+KAFKA_BROKER = os.getenv('CKN_KAFKA_BROKER', 'localhost:9092')
 KAFKA_TOPIC = os.getenv('CKN_KAFKA_TOPIC', 'oracle-events')
 DEVICE_ID = os.getenv('CAMERA_TRAPS_DEVICE_ID', 'device_1')
 
@@ -20,6 +20,9 @@ header = ["image_count", "UUID", "image_name", "ground_truth",
 
 
 class OracleEventHandler(FileSystemEventHandler):
+    """
+    Event handler class to handle events received from the Oracle plugin through the output.json.
+    """
     def __init__(self, file_path, producer, topic, device_id):
         self.file_path = file_path
         self.producer = producer
@@ -31,22 +34,35 @@ class OracleEventHandler(FileSystemEventHandler):
         pass
 
     def on_modified(self, event):
+        """
+        When the file is modified, execute the event handler.
+        """
         if event.src_path == self.file_path:
-            logging.info(f"File {self.file_path} modified.")
+            logging.debug(f"File {self.file_path} modified.")
             self.read_json_events()
 
     def read_csv_lines(self):
-        logging.info(f"Reading new image data from {self.file_path}")
+        """
+        CSV reader for csv files.
+        :return:
+        """
+        logging.debug(f"Reading new image data from {self.file_path}")
         with open(self.file_path, mode='r') as file:
             reader = csv.DictReader(file, fieldnames=header)
             for line_number, row in enumerate(reader, start=1):
                 if line_number not in self.processed_images:
                     self.processed_images.add(line_number)
-                    logging.info(f"New row: {row}")  # or process the row as needed
+                    logging.debug(f"New row: {row}")  # or process the row as needed
                     self.produce_event(row)
 
     def read_json_events(self):
-        logging.info(f"Reading new image data from {self.file_path}")
+        """
+        Reads the JSON events from the file.
+        Only sends events if the image_decision component is present in the JSON entry.
+        This is to make sure only the processed images are extracted.
+        :return:
+        """
+        logging.debug(f"Reading new image data from {self.file_path}")
         # Load the JSON data from the file. If the file is not yet written fully, wait for it to be written.
         while True:
             try:
@@ -110,10 +126,15 @@ class OracleEventHandler(FileSystemEventHandler):
             self.produce_event(event)
 
     def produce_event(self, event):
+        """
+        Adds the device_id to the event and sends it to the CKN broker.
+        :param event:
+        :return:
+        """
         try:
             # add the device id
             event['device_id'] = self.device_id
-            logging.info(f"New row: {event}")
+            logging.info(f"New oracle event: {event}")
             row_json = json.dumps(event)
 
             # send the event
@@ -134,15 +155,20 @@ def setup_logging():
     :return:
     """
     log_formatter = logging.Formatter('%(asctime)s - %(message)s')
-    root_logger = logging.getLogger()
-    root_logger.setLevel(logging.INFO)
 
+    # Create the root logger
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.DEBUG)
+
+    # Logs all INFO, DEBUG and ERROR to the CKN_LOG_FILE
     file_handler = logging.FileHandler(CKN_LOG_FILE)
+    file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(log_formatter)
     root_logger.addHandler(file_handler)
 
-
+    # Logs INFO and ERROR to stdout
     console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(log_formatter)
     root_logger.addHandler(console_handler)
 
@@ -156,9 +182,11 @@ if __name__ == "__main__":
         logging.info(f"Waiting for {ORACLE_EVENTS_FILE} to exist...")
         time.sleep(1)
 
+    # Configure Kafka producer.
     conf = {'bootstrap.servers': KAFKA_BROKER}
     producer = Producer(**conf)
 
+    # Start the event handler for listening to the file modifications.
     event_handler = OracleEventHandler(file_path=ORACLE_EVENTS_FILE, producer=producer, topic=KAFKA_TOPIC, device_id=DEVICE_ID)
     observer = Observer()
     observer.schedule(event_handler, path=os.path.dirname(ORACLE_EVENTS_FILE), recursive=False)
