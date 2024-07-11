8bebfd45d8abcd358b8ef1ccac8f4799b0b8fc6f
Sachith Withana <swsachith@gmail.com>
2024-07-11 14:35:25 -0400

    adding a test for Kafka connection.

--- oracle_ckn_daemon/oracle_daemon.py
+++ oracle_ckn_daemon/oracle_daemon.py
@@ -6,23 +6,25 @@ import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from confluent_kafka import Producer, KafkaError
+from confluent_kafka.admin import AdminClient
 
-ORACLE_EVENTS_FILE = os.getenv('ORACLE_CSV_PATH', '/Users/swithana/git/icicle/camera-traps/releases/0.3.3/oracle_plugin_dir/image_mapping_final.json')
+ORACLE_EVENTS_FILE = os.getenv('ORACLE_CSV_PATH',
+                               '/Users/swithana/git/icicle/camera-traps/releases/0.3.3/oracle_plugin_dir/image_mapping_final.json')
 CKN_LOG_FILE = os.getenv('CKN_LOG_FILE', './ckn_daemon.log')
 KAFKA_BROKER = os.getenv('CKN_KAFKA_BROKER', 'localhost:9092')
 KAFKA_TOPIC = os.getenv('CKN_KAFKA_TOPIC', 'oracle-events')
 DEVICE_ID = os.getenv('CAMERA_TRAPS_DEVICE_ID', 'device_1')
 
-
 header = ["image_count", "UUID", "image_name", "ground_truth",
-                  "image_receiving_timestamp", "image_scoring_timestamp", "model_id",
-                  "label", "probability", "image_store_delete_time", "image_decision"]
+          "image_receiving_timestamp", "image_scoring_timestamp", "model_id",
+          "label", "probability", "image_store_delete_time", "image_decision"]
 
 
 class OracleEventHandler(FileSystemEventHandler):
     """
     Event handler class to handle events received from the Oracle plugin through the output.json.
     """
+
     def __init__(self, file_path, producer, topic, device_id):
         self.file_path = file_path
         self.producer = producer
@@ -149,6 +151,7 @@ class OracleEventHandler(FileSystemEventHandler):
         except KafkaError as e:
             logging.error(f"Kafka error: {e}")
 
+
 def setup_logging():
     """
     Logs to both console and file.
@@ -173,6 +176,26 @@ def setup_logging():
     root_logger.addHandler(console_handler)
 
 
+def test_ckn_broker_connection(configuration, timeout=10):
+    """
+    Checks if the CKN broker is up and running.
+    :param bootstrap_servers: CKN broker hosts
+    :param timeout: seconds to wait for the admin client to connect
+    :return:
+    """
+    while True:
+        try:
+            admin_client = AdminClient(configuration)
+            # Access the topics, if not successful wait
+            topics = admin_client.list_topics(timeout=timeout)
+            return True
+        except Exception as e:
+            logging.info(f"CKN broker not available yet: {e}. Retrying in 5 seconds...")
+            time.sleep(5)
+
+
+
+
 if __name__ == "__main__":
     setup_logging()
     logging.basicConfig(filename=CKN_LOG_FILE, level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -183,11 +206,19 @@ if __name__ == "__main__":
         time.sleep(1)
 
     # Configure Kafka producer.
-    conf = {'bootstrap.servers': KAFKA_BROKER}
-    producer = Producer(**conf)
+    kafka_conf = {'bootstrap.servers': KAFKA_BROKER, 'log_level': 0}
+
+    logging.info("Connecting to the CKN broker at %s", KAFKA_BROKER)
+    # Wait for CKN broker to be available
+    test_ckn_broker_connection(kafka_conf)
+    logging.info("Successfully connected to the CKN broker at %s", KAFKA_BROKER)
+
+    # Initialize the Kafka producer
+    producer = Producer(**kafka_conf)
 
     # Start the event handler for listening to the file modifications.
-    event_handler = OracleEventHandler(file_path=ORACLE_EVENTS_FILE, producer=producer, topic=KAFKA_TOPIC, device_id=DEVICE_ID)
+    event_handler = OracleEventHandler(file_path=ORACLE_EVENTS_FILE, producer=producer, topic=KAFKA_TOPIC,
+                                       device_id=DEVICE_ID)
     observer = Observer()
     observer.schedule(event_handler, path=os.path.dirname(ORACLE_EVENTS_FILE), recursive=False)
 
