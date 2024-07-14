92e64e42ceede0ab558fb62df9b63d202a4723b8
neelk <neelesh.karthi@gmail.com>
2024-07-14 11:53:44 -0700

    debugged kafka event consumer

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,84 +1,43 @@
-#!/usr/bin/env python
 import json
-from datetime import datetime
 import threading
 import panel as pn
+from confluent_kafka import Consumer
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
-from confluent_kafka import Consumer, KafkaError
 
-pn.extension()
+pn.extension(sizing_mode="stretch_width")
 
-messages = []
-
+streaming_component = pn.pane.JSON()
 checkbox = pn.widgets.Checkbox(name='Show saved events only')
-log_pane = pn.widgets.TextAreaInput(name='All Events', value='', height=300, sizing_mode='stretch_width', disabled=True)
-alerts_pane = pn.widgets.TextAreaInput(name='Alerts',value='', height=300, sizing_mode='stretch_width', disabled=True)
 
-# ColumnDataSource for the Bokeh plot
 source = ColumnDataSource(data={'time': [], 'probability': []})
 plot = figure(title="Score Probability Over Time", x_axis_type='datetime', height=330, sizing_mode='stretch_width')
 plot.line(x='time', y='probability', source=source, line_width=2)
 plot.xaxis.axis_label = 'Time'
 plot.yaxis.axis_label = 'Score Probability'
 
+# Function to consume messages from Kafka
 def consume_messages():
     config = {
-        'bootstrap.servers': 'localhost:54622',
+        'bootstrap.servers': 'localhost:9092',
         'group.id': 'ckn-analytics-dashboard',
         'auto.offset.reset': 'earliest'
     }
 
     consumer = Consumer(config)
-    consumer.subscribe(["oracle-events"])
+    consumer.subscribe(["oracle-alerts"])
 
-    try:
-        while True:
-            msg = consumer.poll(1.0)
-            if msg is not None:
-                if msg.error():
-                    if msg.error().code() == KafkaError._PARTITION_EOF:
-                        print(f"Reached end of partition: {msg.topic()} [{msg.partition()}] at offset {msg.offset()}")
-                    elif msg.error():
-                        print(f"Consumer error: {msg.error()}")
-                        continue
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is not None and not msg.error():
+            event = json.loads(msg.value().decode('utf-8')) if msg.value() is not None else None
+            if event:
+                print(event)
+                # Update the streaming component with the event in a thread-safe manner
+                pn.state.execute(lambda: update_streaming_component(event))
 
-                event = json.loads(msg.value().decode('utf-8')) if msg.value() is not None else None
-                print(f"Consumed message: {event}")
-                if event:
-                    messages.append(event)
-                    update_log_and_plot(event)
-
-            else:
-                continue
-
-    except KeyboardInterrupt:
-        pass
-    finally:
-        consumer.close()
-
-def update_log(new_message):
-    log_pane.value += json.dumps(new_message) + '\n'
-    log_pane.param.trigger('value')  # Manually trigger the update to the value property
-
-def update_plot(new_message):
-    try:
-        new_data = {
-            'time': [datetime.strptime(new_message["image_scoring_timestamp"], '%Y-%m-%dT%H:%M:%S.%f')],
-            'probability': [float(new_message["probability"])]
-        }
-
-        for key in source.data.keys():
-            if key not in new_data:
-                new_data[key] = source.data[key][-1:]
-
-        pn.state.execute(lambda: source.stream(new_data, rollover=200))
-    except Exception as e:
-        print(f"Error updating plot: {e}")
-
-def update_log_and_plot(new_message):
-    update_log(new_message)
-    update_plot(new_message)
+def update_streaming_component(event):
+    streaming_component.object = json.dumps(event, indent=2)
 
 # Start Kafka consumer in a separate thread
 threading.Thread(target=consume_messages, daemon=True).start()
@@ -86,12 +45,10 @@ threading.Thread(target=consume_messages, daemon=True).start()
 # Create the FastListTemplate
 template = pn.template.FastListTemplate(
     title="CKN Analytics Dashboard",
-    sidebar=[checkbox],
-    main=[
-        pn.Row(pn.Column(log_pane, sizing_mode='stretch_width'),
-        pn.Column(alerts_pane, sizing_mode='stretch_width')),
-        plot,
-    ],
+    sidebar=[pn.Column(checkbox)],
+    main=[pn.Card(streaming_component, title="Alerts", collapsible=False),
+          plot],
+    logo="https://www.iu.edu/images/brand/brand-expression/iu-trident-promo.jpg",
     accent="#990000"
 )
 
