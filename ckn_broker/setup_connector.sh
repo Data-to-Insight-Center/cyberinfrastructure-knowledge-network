557b871d63d94568371e6b487de31475c91bc79a
Sachith Withana <swsachith@gmail.com>
2024-07-15 13:41:27 -0400

    addingkafka connector for alerts and adding dashboard to docker-compose

--- ckn_broker/setup_connector.sh
+++ ckn_broker/setup_connector.sh
@@ -20,6 +20,7 @@ echo "Kafka Connect is ready!"
 
 echo "Creating Neo4j Sink Connector"
 curl -X POST -H "Content-Type: application/json" --data @/app/neo4jsink-oracle-events-connector.json http://localhost:8083/connectors
+curl -X POST -H "Content-Type: application/json" --data @/app/neo4jsink-oracle-alerts-connector.json http://localhost:8083/connectors
 
 echo "Connector setup completed"
 
