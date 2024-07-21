386426a3a256600bbef1a771a261ccfc080847d0
neelk <neelesh.karthi@gmail.com>
2024-07-20 22:53:19 -0700

    add interactive widgets

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,6 +1,10 @@
 import os
 import panel as pn
 import pandas as pd
+import datetime as dt
+import panel as pn
+from bokeh.plotting import figure
+
 from dotenv import load_dotenv
 from ckn_kg import CKNKnowledgeGraph
 
@@ -12,39 +16,87 @@ CKN_KG_USER = os.getenv('NEO4J_USER')
 CKN_KG_PWD = os.getenv('NEO4J_PWD')
 
 ckn_kg = CKNKnowledgeGraph(CKN_KG_URI, CKN_KG_USER, CKN_KG_PWD)
+
+
 def load_raw_data(nrows=50):
     latest_edges = ckn_kg.fetch_latest_served_by_edges(nrows)
     df = pd.DataFrame(latest_edges)
     return df
 
-def load_alerts(nrows=10):
-    return ckn_kg.fetch_alerts(nrows)
 
-data = load_raw_data()
-alerts = load_alerts()
+experiment_widget = pn.widgets.MultiChoice(
+    name='Experiment ID', options=ckn_kg.fetch_distinct_experiment_id())
+device_widget = pn.widgets.MultiChoice(name='Edge Device',
+                                       options=ckn_kg.fetch_distinct_devices())
+user_widget = pn.widgets.MultiChoice(name='User',
+                                     options=ckn_kg.fetch_distinct_users())
+date_range_picker = pn.widgets.DatetimeRangePicker(name='Date Range',
+                                                   value=(dt.date(2024, 7, 18),
+                                                          dt.date(2024, 7,
+                                                                  19)))
+decision_widget = pn.widgets.RadioButtonGroup(name='Image Decision',
+                                              options=['Saved', 'Deleted'],
+                                              align="center",
+                                              height=40)
 
-accuracy = pn.indicators.Number(name='Accuracy', value=84, format='{value}%')
-users = pn.indicators.Number(name='Users', value=3, format='{value}')
-images = pn.indicators.Number(name='Images', value=45, format='{value}')
-devices = pn.indicators.Number(name='Devices', value=12, format='{value}')
+widgets_panel = pn.layout.FlexBox(pn.Row(experiment_widget, device_widget,
+                                         user_widget, date_range_picker,
+                                         decision_widget),
+                                  align_items='stretch',
+                                  justify_content='space-between')
 
-# Create a FlexBox layout to evenly distribute the space
-indicators = pn.layout.FlexBox(
-    pn.panel(accuracy, sizing_mode='stretch_width'),
-    pn.panel(users, sizing_mode='stretch_width'),
-    pn.panel(images, sizing_mode='stretch_width'),
-    pn.panel(devices, sizing_mode='stretch_width'),
-    align_items='stretch',
-    justify_content='space-between'
-)
+stats = ckn_kg.get_statistics()
+indicators = {
+    'Accuracy':
+    pn.indicators.Number(name='Accuracy',
+                         value=round(stats['average_probability'] * 100),
+                         format='{value}%',
+                         font_size='24pt'),
+    'Users':
+    pn.indicators.Number(name='Users',
+                         value=stats['user_count'],
+                         format='{value}',
+                         font_size='24pt'),
+    'Images':
+    pn.indicators.Number(name='Images',
+                         value=stats['image_count'],
+                         format='{value}',
+                         font_size='24pt'),
+    'Devices':
+    pn.indicators.Number(name='Devices',
+                         value=stats['device_count'],
+                         format='{value}',
+                         font_size='24pt')
+}
 
-alerts_table = pn.widgets.Tabulator(alerts, pagination="local", header_filters=True, layout='fit_data')
+indicators_panel = pn.layout.FlexBox(*[
+    pn.panel(indicator, sizing_mode='stretch_width')
+    for indicator in indicators.values()
+],
+                                     align_items='stretch',
+                                     justify_content='space-between')
 
-main = pn.Column(indicators, pn.Row(alerts_table))
-template = pn.template.FastListTemplate(
-    title="CKN Analytics Dashboard",
-    main=[main],
-    accent="#990000"
-)
+accuracy_trend_df = ckn_kg.fetch_accuracy_trend(date_range_picker.value)
+accuracy_trend_df['image_scoring_timestamp'] = pd.to_datetime(
+    accuracy_trend_df['image_scoring_timestamp'])
+p = figure(title='Accuracy Trend',
+           x_axis_type='datetime',
+           height=400,
+           sizing_mode="stretch_width")
+p.line(accuracy_trend_df['image_scoring_timestamp'],
+       accuracy_trend_df['probability'],
+       line_width=2)
+
+alerts_table = pn.widgets.Tabulator(ckn_kg.fetch_alerts(),
+                                    pagination="local",
+                                    header_filters=True,
+                                    layout='fit_data',
+                                    page_size=10)
+
+main = pn.Column(widgets_panel, indicators_panel, p,
+                 pn.Card(alerts_table, title="Alerts"))
+template = pn.template.FastListTemplate(title="CKN Analytics Dashboard",
+                                        main=[main],
+                                        accent="#990000")
 
 template.servable()
--- dashboard/ckn_kg.py
+++ dashboard/ckn_kg.py
@@ -2,69 +2,168 @@ from datetime import datetime
 from neo4j import GraphDatabase
 import pandas as pd
 
+
 class CKNKnowledgeGraph():
+
     def __init__(self, ckn_uri, ckn_user, ckn_pwd):
         self.driver = GraphDatabase.driver(ckn_uri, auth=(ckn_user, ckn_pwd))
+        self.session = self.driver.session()
 
-    def fetch_latest_served_by_edges(self, limit=100):
+    def close(self):
+        self.session.close()
+
+    def get_statistics(self):
+        queries = {
+            "average_probability":
+            """
+                MATCH (u:User)-[r:SUBMITTED_BY]-(e:Experiment)-[p:PROCESSED_BY]-(i:RawImage)
+                WITH p, apoc.convert.fromJsonList(p.scores) AS scores
+                UNWIND scores AS score
+                WITH p, MAX(toFloat(score.probability)) AS max_probability
+                RETURN avg(max_probability) AS value
+            """,
+            "user_count":
+            """
+                MATCH (u:User)
+                RETURN count(u) AS value
+            """,
+            "image_count":
+            """
+                MATCH (i:RawImage)
+                RETURN count(i) AS value
+            """,
+            "device_count":
+            """
+                MATCH (d:EdgeDevice)
+                RETURN count(d) AS value
+            """
+        }
+        results = {}
+        with self.session.begin_transaction() as tx:
+            for key, query in queries.items():
+                result = tx.run(query).single()
+                results[key] = result["value"] if result else 0
+        return results
+
+    def get_experiment_info(self):
+        query = """
+        MATCH (e:Experiment {experiment_id: "tapis-exp5-3442334"})
+        OPTIONAL MATCH (e)-[sb:SUBMITTED_BY]->(u:User)
+        OPTIONAL MATCH (e)-[eo:EXECUTED_ON]->(ed:EdgeDevice)
+        OPTIONAL MATCH (e)-[ub:USED_BY]->(m:Model)
+        OPTIONAL MATCH (ri:RawImage)-[pb:PROCESSED_BY]->(e)
+        RETURN e, 
+            collect(DISTINCT {user: u, submitted_time: sb.submitted_time}) AS submitted_by,
+            collect(DISTINCT {edge_device: ed, executed_time: eo.submitted_time}) AS executed_on,
+            collect(DISTINCT {model: m, used_start_time: ub.start_time}) AS used_by,
+            collect(DISTINCT {raw_image: ri, processed_by: pb}) AS processed_images"""
+
+        result = self.session.run(query)
+        return [record.data() for record in result]
+
+    def fetch_accuracy_trend(self, date_range, image_saved=True):
+        start_date, end_date = date_range
+        decision_clause = "AND pb.image_decision = 'Save'" if image_saved else ""
         query = f"""
-        MATCH (ri:RawImage)-[r:SERVED_BY]->(es:EdgeServer)
-        RETURN
-          r.image_count AS image_count,
-          r.image_receiving_timestamp AS image_receiving_timestamp,
-          r.image_scoring_timestamp AS image_scoring_timestamp,
-          r.model_id AS model_id,
-          r.scores AS scores,
-          r.image_store_delete_time AS image_store_delete_time,
-          r.image_decision AS image_decision,
-          r.ingestion_timestamp AS ingestion_timestamp,
-          ri.UUID AS UUID,
-          es.device_id AS device_id
-        ORDER BY r.ingestion_timestamp DESC
-        LIMIT {limit}
+        MATCH (ri:RawImage)-[pb:PROCESSED_BY]-(e:Experiment)
+        WHERE pb.image_scoring_timestamp >= datetime("{start_date.isoformat()}") AND pb.image_scoring_timestamp <= datetime("{end_date.isoformat()}")
+        {decision_clause}
+        WITH pb, pb.image_scoring_timestamp AS image_scoring_timestamp, apoc.convert.fromJsonList(pb.scores) AS scores
+        UNWIND scores AS score
+        RETURN pb.image_scoring_timestamp AS image_scoring_timestamp, score.probability AS probability
         """
 
-        with self.driver.session() as session:
-            result = session.run(query)
-            records = [record.data() for record in result]
+        result = self.session.run(query)
+        records = [
+            (self.convert_to_datetime(record["image_scoring_timestamp"]),
+             record["probability"]) for record in result
+        ]
 
-        return records
+        df = pd.DataFrame(records,
+                          columns=["image_scoring_timestamp", "probability"])
+        df = df.sort_values(by='image_scoring_timestamp')
+
+        return df
+
+    def fetch_distinct_users(self):
+        query = """
+        MATCH (u:User)
+        RETURN DISTINCT u.user_id AS user_id
+        """
+
+        result = self.session.run(query)
+        users = [record["user_id"] for record in result]
+
+        return users
+
+    def fetch_distinct_devices(self):
+        query = """
+        MATCH (d:EdgeDevice)
+        RETURN DISTINCT d.device_id AS device_id
+        """
+
+        result = self.session.run(query)
+        devices = [record["device_id"] for record in result]
+
+        return devices
+
+    def fetch_distinct_experiment_id(self):
+        query = """
+        MATCH (e:Experiment)
+        RETURN DISTINCT e.experiment_id AS experiment_id
+        """
+
+        result = self.session.run(query)
+        experiment_ids = [record["experiment_id"] for record in result]
+
+        return experiment_ids
+
+    def fetch_latest_served_by_edges(self, limit=100):
+        query = """
+        MATCH (e:Experiment {experiment_id: "tapis-exp5-3442334"})
+        OPTIONAL MATCH (e)-[sb:SUBMITTED_BY]->(u:User)
+        OPTIONAL MATCH (e)-[eo:EXECUTED_ON]->(ed:EdgeDevice)
+        OPTIONAL MATCH (e)-[ub:USED_BY]->(m:Model)
+        OPTIONAL MATCH (ri:RawImage)-[pb:PROCESSED_BY]->(e)
+        RETURN e, 
+            collect(DISTINCT {user: u, submitted_time: sb.submitted_time}) AS submitted_by,
+            collect(DISTINCT {edge_device: ed, executed_time: eo.submitted_time}) AS executed_on,
+            collect(DISTINCT {model: m, used_start_time: ub.start_time}) AS used_by,
+            collect(DISTINCT {raw_image: ri, processed_by: pb}) AS processed_images
+        LIMIT $limit
+        """
+        result = self.session.run(query, limit=limit)
+        return [record.data() for record in result]
 
     def fetch_alerts(self, limit=100):
-        query = f"""
+        query = """
             MATCH (alert:ALERT)
             RETURN alert
             ORDER BY alert.timestamp DESC
-            LIMIT {limit}
+            LIMIT $limit
             """
-        with self.driver.session() as session:
-            result = session.run(query)
-            records = [record["alert"] for record in result]
+        result = self.session.run(query, limit=limit)
+        records = [record["alert"] for record in result]
 
-        alerts = []
-        for record in records:
-            alert_data = {key: record[key] for key in record.keys()}
-            alerts.append(alert_data)
-
-        if len(alerts) < 1:
+        if not records:
             return pd.DataFrame()
 
-        df = pd.DataFrame(alerts)
-
-        # Convert Neo4j DateTime to pandas datetime
+        df = pd.DataFrame(records)
         df['timestamp'] = df['timestamp'].apply(self.convert_to_datetime)
-
-        # Reorder the columns
-        df = df[['timestamp', 'alert_name', 'priority', 'source_topic', 'description', 'UUID', 'event_data']]
-
-        # Set timestamp as index
+        df = df[[
+            'timestamp', 'alert_name', 'priority', 'source_topic',
+            'description', 'UUID', 'event_data'
+        ]]
         df.set_index('timestamp', inplace=True)
 
         return df
 
     def convert_to_datetime(self, neo4j_datetime):
-        return datetime(
-            neo4j_datetime.year, neo4j_datetime.month, neo4j_datetime.day,
-            neo4j_datetime.hour, neo4j_datetime.minute, int(neo4j_datetime.second),
-            int(neo4j_datetime.nanosecond / 1000), tzinfo=neo4j_datetime.tzinfo
-        )
+        return datetime(neo4j_datetime.year,
+                        neo4j_datetime.month,
+                        neo4j_datetime.day,
+                        neo4j_datetime.hour,
+                        neo4j_datetime.minute,
+                        int(neo4j_datetime.second),
+                        int(neo4j_datetime.nanosecond / 1000),
+                        tzinfo=neo4j_datetime.tzinfo)
