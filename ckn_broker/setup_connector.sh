c365f26c8d2bacaf1f7d039e9d97263770c1002b
neelk <neelesh.karthi@gmail.com>
2024-08-30 14:46:40 -0400

    revert setup_connector.sh changes

--- ckn_broker/setup_connector.sh
+++ ckn_broker/setup_connector.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Load the configuration from .env file
-config_file="../.env"
-NEO4J_URI=$(grep 'NEO4J_URI' $config_file | awk '{print $2}')
-NEO4J_USER=$(grep 'NEO4J_USER' $config_file | awk '{print $2}')
-NEO4J_PWD=$(grep 'NEO4J_PWD' $config_file | awk '{print $2}')
-
 echo "Installing connector plugins"
 confluent-hub install --no-prompt neo4j/kafka-connect-neo4j:latest
 
@@ -24,20 +18,11 @@ done
 
 echo "Kafka Connect is ready!"
 
-# Function to create Neo4j Sink Connector
-create_connector() {
-  local connector_file=$1
-  curl -X POST -H "Content-Type: application/json" \
-    --data @<(cat $connector_file | jq --arg uri "$NEO4J_URI" --arg user "$NEO4J_USER" --arg pwd "$NEO4J_PWD" \
-      '.config += {"neo4j.server.uri": $uri, "neo4j.authentication.basic.username": $user, "neo4j.authentication.basic.password": $pwd}') \
-    http://localhost:8083/connectors
-}
-
-# Create connectors using the function
-create_connector "/app/neo4jsink-oracle-events-connector.json"
-create_connector "/app/neo4jsink-oracle-alerts-connector.json"
-create_connector "/app/neo4jsink-compiler-data-connector.json"
-create_connector "/app/neo4jsink-cameratraps-power-summary-connector.json"
+echo "Creating Neo4j Sink Connector"
+curl -X POST -H "Content-Type: application/json" --data @/app/neo4jsink-oracle-events-connector.json http://localhost:8083/connectors
+curl -X POST -H "Content-Type: application/json" --data @/app/neo4jsink-oracle-alerts-connector.json http://localhost:8083/connectors
+curl -X POST -H "Content-Type: application/json" --data @/app/neo4jsink-compiler-data-connector.json http://localhost:8083/connectors
+curl -X POST -H "Content-Type: application/json" --data @/app/neo4jsink-cameratraps-power-summary-connector.json http://localhost:8083/connectors
 
 echo "Connector setup completed"
 
