10743e919263ea3f1c6f396f49e9a77d61e8d4cb
neelk <neelesh.karthi@gmail.com>
2024-10-09 20:29:16 -0400

    add postgres to docker-compose.yml

---
+++ plugins/power_monitoring/cloud/ckn_broker/setup_connector.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Installing connector plugins"
+confluent-hub install confluentinc/kafka-connect-jdbc:10.6.2
+
+echo "Verifying installed plugins"
+curl -s http://localhost:8083/connector-plugins | jq
+
+echo "Launching Kafka Connect worker"
+/etc/confluent/docker/run &
+
+echo "Waiting for Kafka Connect to start listening on localhost"
+while : ; do
+  curl_status=$(curl -s -o /dev/null -w %{http_code} http://localhost:8083/connectors)
+  echo -e $(date) " Kafka Connect listener HTTP state: " $curl_status " (waiting for 200)"
+  if [ $curl_status -eq 200 ] ; then
+    break
+  fi
+  sleep 5
+done
+
+echo "Kafka Connect is ready!"
+
+echo "Creating Postgres Sink Connector"
+curl -X POST -H "Content-Type: application/json" --data @/app/postgres-sink-ckn-raw-connector.json http://localhost:8083/connectors
+
+echo "Connector setup completed"
+
+# Keep the container running
+tail -f /dev/null
